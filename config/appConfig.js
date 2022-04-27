400fdb9e9f70d1ed90bee7609bccfdf7d95716f5
Juan Alejandro Flores Pérez <jflores@bancomext.gob.mx>
2022-04-27 13:37:47 -0500

    Cambia Endpoint Login

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,17 +1,5 @@
-
-// backend
-// let backend= process.env.BACK_ENDPOINT
-// let loginEndpoint = process.env.LOGIN_ENDPOINT
-
-// if(!backend) {
-//     backend = "http://localhost:8081/"
-// }
-// if(!loginEndpoint) {
-//     loginEndpoint = "http://localhost:3000"
-// }
-
 let backend = "http://localhost:8081/"
-let loginEndpoint = "http://localhost:3000"
+let loginEndpoint = "https://bancadigital.azurewebsites.net"
 
 
 console.log({ backend, loginEndpoint })
